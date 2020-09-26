2751a0af967394c5735760d4c762026d41df2407
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2020-09-26 17:18:08 -0500

    Update Item feature

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -2,32 +2,37 @@ import React, {useState} from 'react'
 import AddItem from './AddItem';
 import TodoItem from './TodoItem';
 
+export const TodoContext = React.createContext()
+
 const Todo = () => {
     const [list, setList] = useState([])
     const onAddClick = (item) => {
-        console.log('onAddClick')
         setList([...list, item])
     }
     const onDeleteClick = (index) => {
-        console.log('onDeleteClick', index)
         const updatedList = [...list]
         updatedList.splice(index, 1)
         setList(updatedList)
     }
+    const onUpdate = (index, value) => {
+        const updatedList = [...list]
+        updatedList[index] = value
+        setList(updatedList)
+    }
     return (
-        <>
+        <TodoContext.Provider value={{delete: onDeleteClick, update: onUpdate}}>
             <AddItem onAddClick={onAddClick}/>
-            <DisplayList list={list} onDeleteClick={onDeleteClick}/>
-        </>
+            <DisplayList list={list}/>
+        </TodoContext.Provider>
     )
 }
 
-const DisplayList = ({list, onDeleteClick}) => (
-    <ul>
-    {list.map((it,i) => (
-        <TodoItem key={i} item={it} onDeleteClick={onDeleteClick} index={i}/>
+const DisplayList = ({list}) => (
+    <div>
+    {list.map((item,i) => (
+        <TodoItem key={i} item={item} index={i}/>
     ))}
-   </ul>
+   </div>
 )
 
 export default Todo
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { TodoContext } from './Todo'
 
-const TodoItem = ({item, onDeleteClick, index}) => {
+const TodoItem = ({item, index}) => {
+    const todoContext = useContext(TodoContext);
+    const [isUpdateMode, setIsUpdateMode] = useState(false)
+    const [itemText, setItemText] = useState(item)
+    const onItemTextChange = (e) => {
+        setItemText(e.target.value)
+    }
     return (
         <li>
-            <span>{item}</span>
-            <button onClick={() => onDeleteClick(index)}>delete</button>
+            {!isUpdateMode ?
+            <>
+                <ItemText item={itemText} />
+                <button onClick={() => todoContext.delete(index)}>Delete Item</button>
+                <button onClick={() => setIsUpdateMode(true)}>Update Item</button>
+            </>
+            :
+            <>
+                <ItemInput item={item} onItemTextChange={onItemTextChange}/>
+                <button onClick={() => {
+                    setIsUpdateMode(false)
+                    todoContext.update(index, itemText)
+                }}>Confirm Update</button>
+                <button onClick={() => setIsUpdateMode(false)}>Cancel Update</button>
+            </>
+            }
         </li>
     )
+}
+
+const ItemText = ({ item }) => (
+    <span>{item}</span>
+)
+
+const ItemInput = ({ item, onItemTextChange }) => {
+    return <input defaultValue={item} onChange={onItemTextChange}/>
 }
 
 export default TodoItem
