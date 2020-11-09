6726fa4dec21758af8b8feae4907b9a7c98695fe
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2020-11-09 15:53:14 -0600

    useDispatch and useSelector

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,29 +1,31 @@
 import React, {useEffect} from 'react'
-import { connect } from 'react-redux'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import { increment, decrement, incrementAsync, getData } from '../actions/actionCreator'
 
-export const Counter = ({ count, fetchData, loading, error, decrement, incrementAsync, data }) => {
+export const Counter = (props) => {
+    const { count, data, loading, error } = useSelector((state) => (state.counter))
+    const dispatch = useDispatch()
     useEffect(() => {
-        fetchData(count)
-    }, [fetchData, count])
+        dispatch(getData(count))
+    }, [count])
     return (
         <div>
             <button
                 aria-label="Increment value"
-                onClick={() => incrementAsync(10)}
+                onClick={() => dispatch(incrementAsync(10))}
             >
             +
             </button>
             <span>{count}</span>
             <button
                 aria-label="Decrement value"
-                onClick={() => decrement()}
+                onClick={() => dispatch(decrement())}
             >
             -
             </button>
             <button
                 aria-label="Decrement value"
-                onClick={() => fetchData(count)}
+                onClick={() => dispatch(getData(count))}
             >
             GET DATA AGAIN
             </button>
@@ -33,22 +35,22 @@ export const Counter = ({ count, fetchData, loading, error, decrement, increment
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        count: state.counter.count,
-        data: state.counter.data,
-        loading: state.counter.loading, 
-        error: state.counter.error
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         count: state.counter.count,
+//         data: state.counter.data,
+//         loading: state.counter.loading, 
+//         error: state.counter.error
+//     }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: () => dispatch(increment()),
-        decrement: () => dispatch(decrement()),
-        incrementAsync: (amount) => dispatch(incrementAsync(amount)),
-        fetchData: (userId) => dispatch(getData(userId))
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         increment: () => dispatch(increment()),
+//         decrement: () => dispatch(decrement()),
+//         incrementAsync: (amount) => dispatch(incrementAsync(amount)),
+//         fetchData: (userId) => dispatch(getData(userId))
+//     }
+// }
 
-export default connect (mapStateToProps, mapDispatchToProps) (Counter)
+export default connect () (Counter)
